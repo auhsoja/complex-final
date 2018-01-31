726d49ef5a2ba8be5d36e21b8cd381ff23e4b3e9
Matt Mims <matthewrmims@gmail.com>
2018-01-31 12:05:46 -0500

    Added robby the robot code, need to test robot class

--- PSO.py
+++ PSO.py
@@ -1,13 +1,17 @@
 import numpy as np
-import sys
+import time
 
-xRange = [-2., 2.]
-yRange = [-2., 2.]
+xRange = [0., 2.]
+yRange = [0., 2.]
+aRange = [0., 2.]
+bRange = [0., 2.]
 
 def z(xy): #function to optimize
     x = xy[0]
     y = xy[1]
-    return np.sin(x)*np.sin(y)
+    a = xy[2]
+    b = xy[3]
+    return np.sin(x)*np.sin(y)*np.sin(a)*np.sin(b)
     #return 7*x*y/(np.e**(x**2+y**2))
 
 class PSO():
@@ -37,16 +41,18 @@ class PSO():
             U = self.domain[i][1]
             if self.pos[i] < L:
                 self.pos[i] += U - L
+                self.pos[i] = self.pos[i] % (U-L)
             elif self.pos[i] > U:
                 self.pos[i] += L - U
+                self.pos[i] = self.pos[i] % (U-L)
 
 def init(n):
     swarm = []
     for i in range(n):
-        swarm.append(PSO(2, z, [xRange, yRange]))
+        swarm.append(PSO(4, z, [xRange, yRange, aRange, bRange]))
     return swarm
 
-swarm = init(10)
+swarm = init(100)
 w = 0.98 #inertia parameter
 c1 = 0.02 #personal best acceleration parameter
 c2 = 0.04 #global best acceleration parameter
@@ -56,13 +62,14 @@ for i in range(len(swarm)):
     if swarm[i].best > globalbest:
         globalbest = swarm[i].best
         globalbest_pos = swarm[i].best_pos
+start = time.time()
 tick = 0
-while(tick < 1000):
+while(globalbest < 0.999):
     for i in range(len(swarm)):
         swarm[i].step(globalbest_pos, w, c1, c2)
         if swarm[i].best > globalbest:
             globalbest = swarm[i].best
             globalbest_pos = swarm[i].best_pos
-            print(globalbest, globalbest_pos, tick)
     tick += 1
-print(globalbest, globalbest_pos)
+print(globalbest, globalbest_pos, tick)
+print(time.time() - start)
---
+++ robby_the_robot.py
@@ -0,0 +1,69 @@
+import random
+
+def initGrid(d, n):
+    grid = []
+    grid.append([2]*d) #top wall
+    for i in range(d-2):
+        grid.append([0]*d)
+        grid[i+1][0] = 2 #left wall
+        grid[i+1][d-1] = 2 #right wall
+    grid.append([2]*d) #bot wall
+    while (n > 0): #add n cans
+        a = random.randint(0, len(grid)-1)
+        b = random.randint(0, len(grid)-1)
+        if grid[a][b] == 0:
+            grid[a][b] = 1
+            n -= 1
+    return grid
+
+class robot():
+    def __init__(self, grid, pos = None):
+        if pos == None:
+            self.pos = [random.randint(1, len(grid)-2), random.randint(1, len(grid)-2)]
+        self.score = 0
+        
+    def left(self, grid):
+        if grid[self.pos[0]-1][self.pos[1]] == 2:
+            print("Failed to move left")
+            self.score -= 10
+        else:
+            self.pos[0] -= 1
+
+    def right(self, grid):
+        if grid[self.pos[0]+1][self.pos[1]] == 2:
+            print("Failed to move right")
+            self.score -= 10
+        else:
+            self.pos[0] += 1
+    
+    def up(self, grid):
+        if grid[self.pos[0]][self.pos[1]-1] == 2:
+            print("Failed to move up")
+            self.score -= 10
+        else:
+            self.pos[1] -= 1
+
+    def down(self, grid):
+        if grid[self.pos[0]][self.pos[1]+1] == 2:
+            print("Failed to move down")
+            self.score -= 10
+        else:
+            self.pos[1] += 1
+            
+    def random(self, grid):
+        i = random.randint(1, 4)        
+        if i == 1: self.left(grid)
+        elif i == 2: self.right(grid)
+        elif i == 3: self.up(grid)
+        elif i == 4: self.down(grid)
+            
+    def pickupCan(self, grid):
+        if grid[self.pos[0]][self.pos[1]] == 1:
+            grid[self.pos[0]][self.pos[1]] = 0
+            self.score += 10
+        else:
+            print("Failed to pick up can")
+            self.score -= 10
+
+grid = initGrid(5, 1)
+print(grid())
